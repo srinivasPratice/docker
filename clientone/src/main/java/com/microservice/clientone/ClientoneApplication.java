e40c5f7dcaf3e3e937bb33104099b343bdfc15e7
srinivasHealthB <76938546+srinivasHealthB@users.noreply.github.com>
2021-02-03 23:49:57 +0530

    new

---
+++ clientone/src/main/java/com/microservice/clientone/ClientoneApplication.java
@@ -0,0 +1,15 @@
+package com.microservice.clientone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ClientoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientoneApplication.class, args);
+    }
+
+}
